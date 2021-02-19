70de58fc0796896892513c0ecdb4a26fc39f3f6d
Samoylov Zakhar <samoilov.zahar20121@gmail.com>
2021-02-19 21:34:49 +0300

    Homework 1.1 completed

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -1,4 +1,4 @@
- /*
+/*
 	Кратко о работе с функциями:
 	1) Значения в скобках - это аргументы, которые мы получаем в момент вызова функции. Их и надо присваивать требуемым в заданиях переменным.
 	2) После ключевого слова return вместо комментария необходимо написать переменную либо выражение с ответом.
@@ -6,22 +6,26 @@
  */
 
 function averageMark(a, g, p) {
-	
-    // реализуйте здесь задание №1
-    
-	return // ваш ответ;
+  let algebra = a;
+  let geography = g;
+  let physics = p;
+
+  return (algebra + geography + physics) / 3; // ваш ответ;
 }
 
 function sayHello(userName) {
-	
-    // реализуйте здесь задание №2
-    
-	return // ваш ответ;
+  myName = userName;
+  message = `Привет, мир! Меня зовут ${userName}`;
+
+  return message; // ваш ответ;
 }
 
 function calculateFormula() {
-    
-	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
+  // реализуйте здесь задание №3
+  let x = 2;
+  let y = 22;
+  let z = 0;
+  let example = x * y + 5 * z + x - 1;
+
+  return example; // ваш ответ;
 }
