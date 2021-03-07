59f5b424d230d57b828589c539e6d6ce1885069d
Samoylov Zakhar <samoilov.zahar20121@gmail.com>
2021-03-07 11:32:33 +0300

    Homework 1.3 modified and completed

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -4,31 +4,66 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   // код для задачи №1 писать здесь
   // return totalAmount;
 
-  let percentNum = parseInt(percent);
-  let contributionNum = parseInt(contribution);
-  let amountNum = parseInt(amount);
+  let percentNum = Number(percent);
+  let contributionNum = Number(contribution);
+  let amountNum = Number(amount);
   let dateLive = new Date();
 
-  let bodyCredit = amountNum - contributionNum;
-  let dateMonth = (date.getFullYear() - dateLive.getFullYear()) * 12;
-  let percentBid = percentNum / 12 / 100;
+  const bodyCredit = amountNum - contributionNum;
+  const dateYear = (date.getFullYear() - dateLive.getFullYear()) * 12;
+  const dateMonth = date.getMonth() - dateLive.getMonth();
+  const dateDay = date.getDate() - dateLive.getDate();
+  const percentBid = percentNum / 12 / 100;
 
-  let monthPayment =
+  // console.log(dateYear);
+  // console.log(dateMonth);
+  // console.log(dateDay);
+
+  const monthPayment =
     bodyCredit *
-    (percentBid + percentBid / ((1 + percentBid) ** dateMonth - 1));
+    (percentBid +
+      percentBid /
+        ((1 + percentBid) ** (dateYear || dateMonth || dateDay) - 1));
 
-  let sum = monthPayment * dateMonth;
-  console.log(sum.toFixed(2));
-  return +sum.toFixed(2);
+  const sum = monthPayment * (dateYear || dateMonth || dateDay);
+
+  if (isNaN(percentNum) || percentNum <= 0 || percent === '') {
+    return `Параметр ${
+      percent || '"Процентная ставка"'
+    } содержит неправильное значение ${percent}`;
+  } else if (
+    isNaN(contributionNum) ||
+    contributionNum < 0 ||
+    contribution === ''
+  ) {
+    return `Параметр ${
+      contribution || '"Начальный взнос"'
+    } содержит неправильное значение ${contribution}`;
+  } else if (isNaN(amountNum) || amountNum < 0 || amount === '') {
+    return `Параметр ${
+      amount || '"Общая стоимость"'
+    } содержит неправильное значение ${amount}`;
+  } else if (date < dateLive) {
+    return `Параметр ${date.toLocaleDateString(
+      'ru'
+    )} содержит неправильное значение.`;
+  } else if (isNaN(date)) {
+    return `Параметр ${'"Срок ипотеки"'} содержит неправильное значение.`;
+  } else if (contribution > amount) {
+    return `Зачем вам ипотека? Разница: ${Math.abs(+sum.toFixed(2))}`;
+  } else {
+    console.log(sum.toFixed(2));
+    return +sum.toFixed(2);
+  }
 }
 
 function getGreeting(name) {
   // код для задачи №2 писать здесь
   // return greeting;
 
-  if (name === '' || name === undefined) {
-    return `Привет, мир! Меня зовут Аноним.`;
-  } else {
-    return `Привет, мир! Меня зовут ${name}.`;
+  console.log(name);
+
+  if (name === '' || name === undefined || name) {
+    return `Привет, мир! Меня зовут ${name || 'Аноним'}.`;
   }
 }
