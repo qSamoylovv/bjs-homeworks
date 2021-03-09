ab79923f5fd1a2be3e781adb864aa76917f7b2dd
Samoylov Zakhar <samoilov.zahar20121@gmail.com>
2021-03-09 03:04:00 +0300

    Homework 2.1 completed

---
+++ 2.1-functions/task.js
@@ -0,0 +1,135 @@
+'use strict';
+
+// Задача 1
+
+function getSolutions(a, b, c) {
+  let D = b ** 2 - 4 * a * c;
+  let x1;
+  let x2;
+
+  if (D === 0) {
+    x1 = -b / (2 * a);
+    return { D, roots: [x1] };
+  } else if (D > 0) {
+    x1 = (-b + Math.sqrt(D)) / (2 * a);
+    x2 = (-b - Math.sqrt(D)) / (2 * a);
+    return { D, roots: [x1, x2] };
+  }
+
+  return { D, roots: [] };
+}
+
+function showSolutionsMessage(a, b, c) {
+  let result = getSolutions(a, b, c);
+  console.log(`Вычисляем корни квадратного уравнения ${a}x² + ${b}x + ${c}`);
+  console.log(`Значение дискриминанта: ${result.D}`);
+
+  if (result.roots.length === 0) {
+    return console.log(`Уравнение не имеет вещественных корней`);
+  } else if (result.roots.length === 1) {
+    return console.log(`Уравнение имеет один корень X₁ = ${result.roots[0]}`);
+  } else if (result.roots.length === 2) {
+    return console.log(
+      `Уравнение имеет два корня. X₁ = ${result.roots[0]}, X₂ = ${result.roots[1]}`
+    );
+  }
+}
+
+showSolutionsMessage(2, 4, 2);
+
+// Задача 2
+
+function getAverageScore(data) {
+  let averageScore;
+  let sumData = 0;
+
+  for (let key in data) {
+    // console.log('data.' + key + ' = ' + data[key]);
+    // console.log(data[key]);
+    let sumDataKeys = 0;
+
+    for (let i = 0; i < data[key].length; i++) {
+      sumDataKeys += data[key][i];
+    }
+
+    let averageSumDataKeys = sumDataKeys / data[key].length;
+
+    data[key] = averageSumDataKeys;
+    // console.log(sumDataKeys);
+    // console.log(averageSumDataKeys);
+    sumData += data[key];
+    // console.log(sumData);
+    averageScore = sumData / Object.values(data).length;
+  }
+
+  if (Object.values(data).length === 0) {
+    averageScore = 0;
+  }
+
+  data.average = averageScore;
+
+  for (let viewResult in data) {
+    console.log('data.' + viewResult + ' = ' + data[viewResult]);
+  }
+
+  // console.log(averageScore);
+  // console.log(data);
+
+  return data;
+}
+
+let resultGetScore = getAverageScore({
+  algebra: [2, 4, 5, 2, 3, 4],
+  geometry: [3, 4, 5],
+  russian: [3, 3, 4, 5],
+  physics: [5, 5],
+  music: [2, 2, 6],
+  english: [4, 4, 3],
+  poetry: [5, 3, 4],
+  chemistry: [2],
+  french: [4, 4],
+});
+
+function getAverageMark(marks) {
+  let sum = 0;
+
+  if (marks.length === 0) {
+    return 0;
+  } else {
+    for (let j = 0; j < marks.length; j++) {
+      sum += marks[j];
+    }
+  }
+
+  return sum / marks.length;
+}
+
+getAverageMark(resultGetScore);
+
+// Задача 3
+
+let getPersonData = (secretData) => Object.values(secretData);
+
+let personData = getPersonData({ aaa: 0, bbb: 1 });
+
+function getDecodedValue(secret) {
+  if (secret[0] === 0 && secret[1] === 1) {
+    secret[0] = 'Родриго';
+    secret[1] = 'Эмильо';
+  } else if (secret[0] === 1 && secret[1] === 0) {
+    secret[0] = 'Эмильо';
+    secret[1] = 'Родриго';
+  } else if (secret[0] === 0 && secret[1] === 0) {
+    secret[0] = 'Родриго';
+    secret[1] = 'Родриго';
+  } else if (secret[0] === 1 && secret[1] === 1) {
+    secret[0] = 'Эмильо';
+    secret[1] = 'Эмильо';
+  }
+
+  let secretObject = { firstName: secret[0], lastName: secret[1] };
+
+  return secretObject;
+}
+
+console.log(getDecodedValue(personData));
